2ffb4f7a16ea3f1cff6b53e3188f18e8b145b1f5
FDUTCH <para.trabajos9999@gmail.com>
2025-03-15 19:03:15 +0100

    Revert "fix"
    This reverts commit 07abc346fc87ed51d1a03ff56a96ff5e6e4027ea.

--- internal/pool.go
+++ internal/pool.go
@@ -2,8 +2,6 @@ package internal
 
 import (
 	"bytes"
-	packet2 "github.com/cooldogedev/spectrum/server/packet"
-	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
 	"sync"
 )
 
@@ -11,13 +9,4 @@ var BufferPool = sync.Pool{
 	New: func() any {
 		return bytes.NewBuffer(make([]byte, 0, 256))
 	},
-}
-
-func addPackets(pool packet.Pool) {
-	pool[packet2.IDConnectionRequest] = func() packet.Packet { return &packet2.ConnectionRequest{} }
-	pool[packet2.IDLatency] = func() packet.Packet { return &packet2.Latency{} }
-
-	pool[packet2.IDConnectionResponse] = func() packet.Packet { return &packet2.ConnectionResponse{} }
-	pool[packet2.IDLatency] = func() packet.Packet { return &packet2.Latency{} }
-	pool[packet2.IDTransfer] = func() packet.Packet { return &packet2.Transfer{} }
 }
